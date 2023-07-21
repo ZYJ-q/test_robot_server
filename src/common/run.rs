ee27743f333ed75b30a5182943bf23b952dd91e4
ZYJ-q <2979049853@qq.com>
2023-07-21 10:17:25 +0800

    p

--- src/common/run.rs
+++ src/common/run.rs
@@ -8,7 +8,7 @@ use super::http::handlers;
 
 
 pub async fn server(ip: String, config_db: HashMap<String, String>) -> std::io::Result<()> {
-    log::info!("starting HTTP server at http://{}:8081", &ip);
+    log::info!("starting HTTP server at http://{}:8082", &ip);
 
     let pool = database::create_pool(config_db);
 
@@ -70,7 +70,7 @@ pub async fn server(ip: String, config_db: HashMap<String, String>) -> std::io::
             .service(web::resource("/get_papi_open_orders").route(web::post().to(handlers::papi_open_orders)))
             .service(web::resource("/get_papi_klines").route(web::post().to(handlers::papi_klines)))
     })
-    .bind((ip.as_str(), 8081))?
+    .bind((ip.as_str(), 8082))?
     .run();
 
     return server.await;
